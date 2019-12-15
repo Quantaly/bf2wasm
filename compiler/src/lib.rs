61dfb15f9ddc2bdf6e979e62440ead8f3fb9e79c
Kai Page <kaibug@gmail.com>
2019-12-14 21:12:36 -0600

    Parser + AST for Brainfuck

---
+++ compiler/src/lib.rs
@@ -0,0 +1,26 @@
+pub mod parser;
+
+pub use parser::parse;
+
+/// Elements of Brainfuck's syntax.
+#[derive(Debug, PartialEq)]
+pub enum BrainfuckSyntax {
+    /// Represents some number of contiguous `<` or `>` commands.
+    MovePointer(i32),
+    /// Represents some number of contiguous `+` or `-` commands.
+    ModifyValue(i32),
+    /// Represents a `.` command.
+    Output,
+    /// Represents a `,` command.
+    Input,
+    /// Represents a `[`, its matching `]`, and all commands in between.
+    Loop(Vec<BrainfuckSyntax>),
+}
+
+/// If this test fails, all the rest are pretty (brain)fucking useless.
+#[test]
+fn syntax_equality() {
+    use BrainfuckSyntax::*;
+    assert_eq!(vec![Input, Output, MovePointer(4), Loop(vec![Input])], vec![Input, Output, MovePointer(4), Loop(vec![Input])]);
+    assert_ne!(vec![Output, Input], vec![Input, Output]);
+}
